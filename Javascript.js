1bb117fa34a081493c6648b328c54209090131ea
InMunken <FedericoDaRosa137682@gmail.com>
2023-10-31 23:01:51 -0300

    add. primeras ideas para buscar la palabra

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,48 @@
 const miPalabra = document.getElementById("palabra");
 
-const palabraClave = "Bicicleta";
+const palabraClave = "bicicleta";
 console.log(palabraClave);
 console.log(palabraClave.length);
 
 miPalabra.textContent = palabraClave;
+
+
+const palabraAAdivinar = document.createElement('div')
+palabraAAdivinar.classList.add('palabra-a-adivinar')
+
+miPalabra.appendChild(palabraAAdivinar)
+
+const letras = palabraClave.split(''); 
+
+letras.forEach(letra => {
+
+    const aux = document.createElement('div');
+    aux.textContent = letra;
+    aux.classList.add('letra-ocultada');
+
+    palabraAAdivinar.appendChild(aux);
+});
+
+
+
+
+function estaEnPalabra(letra){
+
+    const letras = palabraClave.split('');
+    return letras.includes(letra)
+
+}
+
+function enDondeEsta(letra){
+    for (let i = 0; i < palabra.length; i++) {
+        if (palabra[i] === letraBuscada) {
+            return i; 
+        }
+    }
+    return -1;
+}
+
+
+console.log(estaEnPalabra('b'))
+
+
